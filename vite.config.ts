72acbcbf04ef59792ef7adfb9c24853460d99790
Li Haiyun <li_haiyun@nyp.edu.sg>
2025-07-24 19:23:52 +0800

    remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   server: {
     port: 3000,
     host: true, // Allow external connections
